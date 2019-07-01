e74e9b7ee65a18168d976307b8707be57bff1168
cutebaby0630 <lovemath0630@gmail.com>
2019-07-01 13:01:37 +0800

    Data getset

---
+++ BookSystem/Models/CodeService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookSystem.Models
+{
+    public class CodeService
+    { 
+        /// <summary>
+        /// 取得DB連線字串
+        /// </summary>
+        /// <returns></returns>
+         private string GetDBConnectionString()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString.ToString();
+         }
+
+        /// <summary>
+        /// 取得書本資料
+        /// </summary>
+        /// <returns></returns>
+        public List<SelectListItem> GetBook(string bookId)
+        {
+            DataTable dt = new DataTable();
+            string sql = @"SELECT bd.BOOK_ID AS CodeId, bd.BOOK_NAME AS CodeName 
+                           FROM BOOK_DATA bd  
+                           WHERE BookID != @Book_ID";
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.Add(new SqlParameter("@Book_ID", bookId));
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+            return this.MapCodeData(dt);
+        }
+
+        /// <summary>
+        /// 取得codeTable的部分資料
+        /// </summary>
+        /// <returns></returns>
+        public List<SelectListItem> GetCodeTable(string type)
+        {
+            DataTable dt = new DataTable();
+            string sql = @"Select Distinct CodeVal As CodeName, CodeId As CodeID 
+                           From dbo.CodeTable 
+                           Where CodeType = @Type";
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.Add(new SqlParameter("@Type", type));
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+            return this.MapCodeData(dt);
+        }
+
+        /// <summary>
+        /// Maping 代碼資料
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        private List<SelectListItem> MapCodeData(DataTable dt)
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(new SelectListItem()
+                {
+                    Text = row["CodeId"].ToString() + '-' + row["CodeName"].ToString(),
+                    Value = row["CodeId"].ToString()
+                });
+            }
+            return result;
+        }
+    }
+   
+}
